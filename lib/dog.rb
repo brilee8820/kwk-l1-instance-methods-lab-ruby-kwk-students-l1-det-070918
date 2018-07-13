d57d9993829d2b08b6fbab473c6f598ffc8ca492
brilee8820 <brilee8820@gmail.com>
2018-07-13 17:29:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# dog.rb
+class Dog 
+end
+
+module Bark 
+  def bark(sound)
+    puts "#{sound}"
+  end
+  
+  class Dog 
+    include bark
+  end 
+  
+  sparky = Dog.new 
+  sparky.bark("Woof!")
+   
+module Sit 
+  def sit(sit)
+    puts "#{sit}"
+  end 
+  
+  sparky = Dog.new
+  sparky.sit("The Dog is sitting")
+    
