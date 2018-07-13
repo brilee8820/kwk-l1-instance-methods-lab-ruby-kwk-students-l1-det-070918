c9faf716947269c9d97c57d790f6bdaef29471b5
brilee8820 <brilee8820@gmail.com>
2018-07-13 17:40:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,32 @@
 class Dog 
+def bark 
+  puts "Woof!"
 end
 
-module Bark 
-  def bark(sound)
-    puts "#{sound}"
-  end
+class Dog
+  def sit 
+    puts "The Dog is sitting "
+# module Bark 
+#   def bark(sound)
+#     puts "#{sound}"
+#   end
   
-  class Dog 
-    include bark
-  end 
+#   class Dog 
+#     include bark
+#   end 
   
-  sparky = Dog.new 
-  sparky.bark("Woof!")
+#   sparky = Dog.new 
+#   sparky.bark("Woof!")
    
-module Sit 
-  def sit(sit)
-    puts "#{sit}"
-  end 
+# module Sit 
+#   def sit(sit)
+#     puts "#{sit}"
+#   end 
   
-  class Sit 
-    include sit 
-  end 
+#   class Sit 
+#     include sit 
+#   end 
   
-  sparky.sit("The Dog is sitting")
-end
+#   sparky.sit("The Dog is sitting")
+# # end
     
